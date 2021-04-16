07ae9f95bdd9c578e279d3750270e594ffd59730
Breno S F <BrenoSilvaFortunato@gmail.com>
2021-04-16 17:43:49 -0300

    structure

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+const categoriesController = require('./categories/CategoriesController');
+const categoriesArticles = require('./articles/articlesController');
+
+// View Engine
+ app.set('view engine', 'ejs');
+
+// Static
+app.use(express.static('public'));
+
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//  Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log('connection was successful')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+// Routes
+app.use('/', categoriesController);
+app.use('/', categoriesArticles);
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(8080, () => {
+    console.log('server started on port 8080')
+});
